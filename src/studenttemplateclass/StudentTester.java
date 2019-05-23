4f4ffaf04f7f5dfbb6acfc176490c071e3cdd95f
kauekamp <50718515+kauekamp@users.noreply.github.com>
2019-05-23 16:39:45 -0400

    Update StudentTester.java

--- src/studenttemplateclass/StudentTester.java
+++ src/studenttemplateclass/StudentTester.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  May 23rd, 2019
- Ekampreet*/
+ Ekampreet
+ Sheridan College*/
 package studenttemplateclass;
 
 /**
